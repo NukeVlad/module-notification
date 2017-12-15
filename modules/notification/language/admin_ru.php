ddc111210d484b1af63ab09a5b592c222aa3d444
NukeVlad <vipvnnru@yahoo.com>
2017-12-15 12:59:19 +0300

    Add files via upload

---
+++ modules/notification/language/admin_ru.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author Mr.Thang (kid.apt@gmail.com) локализация NukeVlad (vipvnnru@yahoo.com)
+ * @Copyright (C) 2017 Mr.Thang. All rights reserved
+ * @License GNU/GPL version 2 or any later version
+ * @Createdate 10-01-2017 20:08
+ */
+
+if ( ! defined( 'NV_MAINFILE' ) )
+{
+    die( 'Stop!!!' );
+}
+
+$lang_translator['author'] = "Mr.Thang (kid.apt@gmail.com)";
+$lang_translator['createdate'] = "07/03/2011, 20:15";
+$lang_translator['copyright'] = "@Copyright (C) 2011 Mr.Thang. All rights reserved";
+$lang_translator['info'] = "локализация NukeVlad (vipvnnru@yahoo.com)";
+$lang_translator['langtype'] = "lang_module";
+
+$lang_module['alias'] = "Статическая ссылка";
+
+$lang_module['add_notification'] = "Отправить новое уведомление";
+$lang_module['notification'] = "Список уведомлений";
+$lang_module['notification_config'] = 'Настройки';
+$lang_module['notification_message'] = "Содержание сообщения";
+$lang_module['notification_url'] = "URL сообщения";
+$lang_module['notification_icon'] = "Иконка уведомления";
+$lang_module['notification_icon_note'] = "Можно использовать Значок на CSS, например, fa fa-user";
+$lang_module['notification_author'] = "Уведомления для отправителя";
+$lang_module['notification_showview'] = "Тип дисплея";
+$lang_module['notification_showview_0'] = "Отображается только один раз.";
+$lang_module['notification_showview_1'] = "Отображает при каждом новом сеансе";
+$lang_module['notification_showview_2'] = 'Показать настройки времени';
+$lang_module['notification_allowed_view'] = "Группы пользователей, получающих уведомления";
+$lang_module['notification_status'] = 'Активировать';
+$lang_module['notification_status_1'] = 'Дисплей';
+$lang_module['notification_status_0'] = 'Скрыть';
+$lang_module['notification_error_message'] = 'Олибка:  Вы не ввели Содержание сообщения!';
+$lang_module['notification_error_save'] = 'Ошибка: данные не сохраены!';
+$lang_module['save'] = 'Сохранить данные';
+$lang_module['addnew_notification'] = 'Добавить новое уведомление';
+$lang_module['update_notification'] = 'Редактирование идентификатора уведомления: ';
+$lang_module['delete_notification'] = 'Удалить сообщение с кодом:';
+
+$lang_module['notification_config_timeview'] = 'Время получения уведомдений';
+$lang_module['notification_config_timeview_0'] = 'Прямо сейчас';
+$lang_module['notification_config_timeview_option'] = 'Займет около %s минут';
+$lang_module['notification_config_timeout'] = 'Время вызова в системе для извлечения информации (в секундах)';
+$lang_module['config'] = 'Настроить модуль';
+$lang_module['notification_addtime'] = 'Уведомление о времени';
